812405cea79b4f83eea80dcc01fb722cb6c05f9d
Yoshi Yamaguchi <ymotongpoo+github@gmail.com>
2023-11-14 18:11:43 +0900

    feat(receiver/slack): add slackreceiver (#18)
    Newly add a metrics receiver for Slack
    
    * Add first minimum working sample of metrics receiver for Slack
    
    Fixes #17

---
+++ receiver/slackreceiver/factory.go
@@ -0,0 +1,52 @@
+// Copyright 2023 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slackreceiver
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/receiver"
+
+	metadata "github.com/ymotongpoo/opentelemetry-collector-extra/receiver/slackreceiver/internal/metadata"
+)
+
+// NewFactory returns a new factory for the Slack receiver.
+func NewFactory() receiver.Factory {
+	return receiver.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+		receiver.WithMetrics(createMetricsReceiver, metadata.MetricsStability),
+	)
+}
+
+func createMetricsReceiver(
+	_ context.Context,
+	settings receiver.CreateSettings,
+	cfg component.Config,
+	consumer consumer.Metrics,
+) (receiver.Metrics, error) {
+	if consumer == nil {
+		return nil, component.ErrNilNextConsumer
+	}
+
+	c := cfg.(*Config)
+	err := c.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return newSlackReceiver(c, settings, consumer)
+}
